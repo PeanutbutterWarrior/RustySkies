d1caca7d05a6c077b896786597f0d1fadf3b10d7
PeanutbutterWarrior <jamesandrewmcclure@yahoo.co.uk>
2022-04-17 20:07:05 +0100

    Optimise writing to files

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,28 @@ mod noise;
 use noise::Point;
 use std::io::Write;
 
+const WIDTH: i64 = 400;
+const HEIGHT: i64 = 400;
+
 fn main() {
     let mut noise_gen = noise::WorleyNoiseGen::new();
 
-    let mut file_out = std::fs::File::create(r"output/out.txt").unwrap();
+    let mut file_out = match std::fs::File::create(r"output/out.txt") {
+        Ok(file) => file,
+        Err(_) => std::fs::File::create("out.txt").unwrap(),
+    };
 
-    for x in 0..=200 {
-        for y in 0..=200 {
-            write!(file_out, "{} {} {}\n", x, y, noise_gen.generate_noise(Point(x, y))).unwrap();
+    let mut output = Vec::with_capacity((WIDTH * HEIGHT) as usize);
+
+    for x in 0..=WIDTH {
+        for y in 0..=HEIGHT {
+            output.push((x, y, noise_gen.generate_noise(Point(x, y))));
         }
     }
-    println!("Finished")
+    println!("Finished generating");
+
+    for (x, y, noise) in output.into_iter() {
+        file_out.write_all(format!("{} {} {}\n", x, y, noise).as_bytes()).unwrap();
+    }
+    println!("Finished writing")
 }
